428ef5f03d863196d41b256325eb66c32b2fe2c0
张峥 <18601254750@163.com>
2017-05-02 23:35:13 +0800

    Update helloworld.c

--- src/helloworld.c
+++ src/helloworld.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <signal.h>
+
+void sigroutine(int dunno) {
+    _exit(1);
+}
 
 int main(int argc, char *argv[])
 {
     char *message = "hello, world\n";
     size_t len = strlen(message);
+    char buf;
 
-    write(1, message, len);
+    while(1) {
+        signal(SIGINT, sigroutine);
+        write(1, message, len);
 
-    _exit(7);
+        read(1, &buf, 1);
+    }
+
+    _exit(0);
 }
